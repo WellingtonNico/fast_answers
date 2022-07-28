c24f9cf4501b9a97c65ebc3daf2a700629d25f69
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2022-07-28 08:50:27 -0300

    melhoria no comprimento do botão

--- lib/models/fast_answer.py
+++ lib/models/fast_answer.py
@@ -7,11 +7,13 @@ from lib.utils import get_salutation
 import pyperclip
 
 
+MAX_TITLE_LENGTH = 30
+MAX_TEXT_LENGTH = 700
 class FastAnswer(BaseModel):
     __tablename__ = 'fastanswer'
-    title:str = Column('título',String(30),nullable=False)
+    title:str = Column('título',String(MAX_TITLE_LENGTH),nullable=False)
     priority_number:int = Column('prioridade',Integer,nullable=False)
-    text:str = Column('texto',String(500),nullable=False)
+    text:str = Column('texto',String(MAX_TEXT_LENGTH),nullable=False)
     text_color:str = Column('cor do texto',String,nullable=False)
     button_color:str = Column('cor do botão',String,nullable=False)
 
@@ -26,10 +28,14 @@ class FastAnswer(BaseModel):
     def validate_title(self):
         if not self.title:
             raise ValidationError('O título não pode estar vazio')
+        if len(self.title) > MAX_TITLE_LENGTH:
+            raise ValidationError(f'O título não pode ter mais que {MAX_TITLE_LENGTH} caracteres')
     
     def validate_text(self):
         if not self.text:
             raise ValidationError('O texto não pode estar vazio')
+        if len(self.text)>MAX_TEXT_LENGTH:
+            raise ValidationError(f'O texto não pode ter mais que {MAX_TEXT_LENGTH} caracteres')
 
     def validate_button_color(self):
         if not self.button_color:
--- screens/fast_answers_list.py
+++ screens/fast_answers_list.py
@@ -8,7 +8,9 @@ screen = Tk()
 screen.title('Respostas Rápidas')
 height = get_screen_height(screen)
 width = get_screen_width(screen)
-screen.geometry(f'{int(width*0.15)}x{int(height*0.60)}+{int(width*0.85/2)}+{int(height*0.4/2)}')
+SCREEN_WIDTH = int(width*0.15)
+SCREEN_HEIGHT = int(height*0.60)
+screen.geometry(f'{SCREEN_WIDTH}x{SCREEN_HEIGHT}+{int(width*0.85/2)}+{int(height*0.4/2)}')
 screen.iconbitmap('./assets/icon.ico')
 screen.resizable(width=False,height=False)
 
@@ -17,4 +19,5 @@ for fastAnswer in FastAnswer().query.all():
     Button(
         screen,text=fastAnswer.title,command=lambda:fastAnswer.copy(screen),
         bg=fastAnswer.button_color,fg=fastAnswer.text_color,font="Consolas 11 bold",
+        width=int(SCREEN_WIDTH*0.75)
     ).pack()
